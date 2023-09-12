0284f0ba1068bf5f4dab5b53038b944506b09cb6
Brando Angulo <bangulo@playtechla.com>
2023-09-12 10:34:20 -0500

    creando clase roles para el control de sesiones

---
+++ curso2/src/main/java/com/seguridadjwt/curso2/entities/Role.java
@@ -0,0 +1,6 @@
+package com.seguridadjwt.curso2.entities;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ curso2/src/main/java/com/seguridadjwt/curso2/entities/Usuarios.java
@@ -0,0 +1,65 @@
+package com.seguridadjwt.curso2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "usuarios")
+public class Usuarios implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nombre;
+    private String correo;
+    private String celular;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
